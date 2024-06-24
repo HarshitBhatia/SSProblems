113af3c33f385710c087567999106a34a8220c6d
harshit <hbhatia999@gmail.com>
2024-06-25 01:04:27 +0530

    Refactoring Bubble Sort Code

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -11,6 +11,7 @@ public class BubbleSort {
         //Bubble sorting begins
         for(int i=0;i< array.length;i++){
             for(int j=i+1;j<array.length;j++){
+                //Sorting at every step by step level.
                 if(array[j]<array[i]){
                     int temp=array[j];
                     array[j]=array[i];
